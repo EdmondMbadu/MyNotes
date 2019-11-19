798ba6453746430beb4053e2f7079db655c39c61
Edmond Mbadu <mbadungoma@gmail.com>
2019-11-19 09:27:45 -0500

    Initial Commit

---
+++ MyNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyNotes
+//
+//  Created by Mbadu, Edmond Ngoma on 11/19/19.
+//  Copyright © 2019 Mbadu, Edmond Ngoma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
